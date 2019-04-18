533c8322a2dfe7636717a8bddf102ee25265cc9f
Federico Resnizky <fresnizky@yahoo.com.ar>
2019-04-18 19:32:33 -0300

    Finished CRUD Operations with Market Products / Real-Time Data with Subscriptions

--- src/components/PayButton.js
+++ src/components/PayButton.js
@@ -1,8 +1,26 @@
 import React from "react";
+import StripeCheckout from "react-stripe-checkout";
 // import { Notification, Message } from "element-react";
 
-const PayButton = () => {
-  return <div>PayButton</div>;
+const stripeConfig = {
+  currency: "USD",
+  publishableAPIKey: "pk_test_u45gFnpf3jZbVWTiLE0JUZQx00GU2M5y6R"
+};
+
+const PayButton = ({ product, user }) => {
+  return (
+    <StripeCheckout
+      email={user.attributes.email}
+      name={product.description}
+      amount={product.price}
+      currency={stripeConfig.currency}
+      stripeKey={stripeConfig.publishableAPIKey}
+      billingAddress={product.shipped}
+      shippingAddress={product.shipped}
+      locale="auto"
+      allowRememberMe={false}
+    />
+  );
 };
 
 export default PayButton;
